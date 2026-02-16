0db3701ba4da4bb0e7ec6c6939138080e75bee73
Evelyn Cristini Oliveira Throssell <cristinioliveira.eo@gmail.com>
2026-02-16 01:56:22 -0300

    feat: get health check endpoint

--- internal/adapters/http/router.go
+++ internal/adapters/http/router.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter(accountHandler *AccountHandler) *gin.Engine {
+func SetupRouter(accountHandler *AccountHandler, healthHandler *HealthHandler) *gin.Engine {
 	router := gin.Default()
 
 	router.POST("/accounts", accountHandler.CreateAccount)
 	router.GET("/accounts/:accountId", accountHandler.GetAccount)
+
+	router.GET("/health", healthHandler.Check)
 
 	return router
 }
